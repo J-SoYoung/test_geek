e6547682f73b6770a0e1d68e92d68824efbcfd4c
thdud <thgml1444@naver.com>
2024-07-15 14:38:53 +0900

    Add : 중고제품 업로드 페이지 구현 - 사용자의 input데이터를 받아 firebase로 저장 - image 미리보기는 blob url을 사용 / firebase업로드는 fileReader의 base64 url사용
    ** 여러장 image는 구현해야 함

--- src/pages/UsedPostUpload.tsx
+++ src/pages/UsedPostUpload.tsx
@@ -1,8 +1,92 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { used_DB } from "../api/usedFirebase";
+import { push, ref, set } from "firebase/database";
 
 const UsedPostUpload: React.FC = () => {
+  const [previewImages, setPreviewImages] = useState<string[]>([]);
+  const [uploadImages, setUploadImages] = useState<string[]>([]);
+  const [itemName, setItemName] = useState("");
+  const [size, setSize] = useState("");
+  const [price, setPrice] = useState<number>(1000);
+  const [quantity, setQuantity] = useState<number>(1);
+  const [shippingIncluded, setShippingIncluded] = useState(false);
+  const [productCondition, setProductCondition] = useState("used");
+  const [description, setDescription] = useState("");
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      const urlFile = URL.createObjectURL(file);
+      console.log(urlFile);
+      setPreviewImages((prevImages) => prevImages.concat(urlFile));
+
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        const base64data = reader.result;
+        if (base64data) {
+          setUploadImages((prevImages) => prevImages.concat(base64data));
+          console.log(base64data);
+        }
+      };
+    }
+  };
+
+  const removeImage = (index: number) => {
+    URL.revokeObjectURL(previewImages[index]); // 이미지 URL 해제
+    setPreviewImages((prevImages) => prevImages.filter((_, i) => i !== index));
+  };
+
+  const onClickUsedItemUpload = () => {
+    const db = used_DB;
+    const usedItemRef = ref(db, "usedItems");
+    const newItems = push(usedItemRef);
+    set(newItems, {
+      description,
+      imageArr: uploadImages,
+      isSales: false,
+      itemId: `${itemName}_${Math.floor(Math.random() * 100)}`,
+      itemName,
+      options: [
+        shippingIncluded ? "배송비 포함" : "배송비 비포함",
+        productCondition === "used" ? "중고상품" : "새상품",
+      ],
+      price,
+      quantity,
+      reviews: [],
+      seller: {
+        sellerId: "seller1",
+        userName: "ahellyer0",
+        nickname: "aclawsley0",
+        userAvatar:
+          "https://robohash.org/autemautemvel.png?size=50x50&set=set1",
+        address: "70140 Sunnyside Plaza",
+        phone: "010-8433-9597",
+      },
+      size,
+    });
+
+    console.log({
+      description,
+      imageArr: uploadImages,
+      isSales: false,
+      itemId: `${itemName}_${Math.floor(Math.random() * 100)}`,
+      itemName,
+      options: [
+        shippingIncluded ? "배송비 포함" : "배송비 비포함",
+        productCondition === "used" ? "중고상품" : "새상품",
+      ],
+      price,
+      quantity,
+      reviews: [],
+      seller: {},
+      size,
+    });
+  };
+
   return (
-    <div className="h-[100vh] w-[600px] p-8 text-left">
+    <div className="h-[100vh] w-[600px] p-8 text-left mb-40">
       <h1 className="text-3xl font-bold mb-5 ">상품등록 </h1>
 
       {/* 사진등록 */}
@@ -11,19 +95,37 @@ const UsedPostUpload: React.FC = () => {
           사진 등록
         </label>
         <div className="flex space-x-2">
-          <div className="w-20 h-20 bg-gray-200 relative flex items-center justify-center">
-            <button className="absolute top-0 right-0 p-1 text-xs text-gray-500">
-              ×
-            </button>
-          </div>
-          <div className="w-20 h-20 bg-gray-200 relative flex items-center justify-center">
-            <button className="absolute top-0 right-0 p-1 text-xs text-gray-500">
-              ×
-            </button>
-          </div>
-          <div className="w-20 h-20 bg-gray-300 flex items-center justify-center text-2xl text-gray-500">
+          <input
+            type="file"
+            multiple
+            onChange={handleImageChange}
+            className="mb-4 hidden"
+            ref={fileInputRef}
+          />
+          <div
+            onClick={() => fileInputRef.current?.click()}
+            className="w-20 h-20 bg-gray-300 flex items-center justify-center text-2xl text-gray-500 cursor-pointer"
+          >
             +
           </div>
+          {previewImages.map((image, index) => (
+            <div
+              key={index}
+              className="w-20 h-20 bg-gray-200 relative flex items-center justify-center"
+            >
+              <img
+                src={image}
+                alt={`uploaded ${index}`}
+                className="object-cover w-full h-full"
+              />
+              <button
+                onClick={() => removeImage(index)}
+                className="absolute top-0 right-0 p-1 text-xs text-gray-500"
+              >
+                ×
+              </button>
+            </div>
+          ))}
         </div>
       </div>
 
@@ -35,6 +137,10 @@ const UsedPostUpload: React.FC = () => {
           </label>
           <input
             type="text"
+            value={itemName}
+            onChange={(e) => {
+              setItemName(e.target.value);
+            }}
             className="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
             placeholder="상품명을 입력하세요"
           />
@@ -44,9 +150,45 @@ const UsedPostUpload: React.FC = () => {
             가격
           </label>
           <input
-            type="text"
+            type="number"
+            value={price}
+            onChange={(e) => {
+              setPrice(Number(e.target.value));
+            }}
+            min={1000}
             className="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
             placeholder="가격을 입력하세요"
+          />
+        </div>
+        <div className="mb-8">
+          <label className="block text-sm font-medium text-gray-700">
+            수량
+          </label>
+          <input
+            type="number"
+            value={quantity}
+            onChange={(e) => {
+              setQuantity(Number(e.target.value));
+            }}
+            min={1}
+            max={30}
+            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
+            placeholder="1"
+          />
+        </div>
+        <div className="mb-8">
+          <label className="block text-sm font-medium text-gray-700">
+            사이즈
+          </label>
+          <input
+            type="text"
+            value={size}
+            onChange={(e) => {
+              setSize(e.target.value);
+            }}
+            max={30}
+            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
+            placeholder="사이즈를 입력하세요"
           />
         </div>
         <div className="mb-8">
@@ -59,8 +201,8 @@ const UsedPostUpload: React.FC = () => {
                 type="radio"
                 name="shipping"
                 value="included"
-                // checked={shippingIncluded}
-                // onChange={() => setShippingIncluded(true)}
+                checked={shippingIncluded}
+                onChange={() => setShippingIncluded(true)}
                 className="form-radio"
               />
               <span className="ml-2">배송비 포함</span>
@@ -70,8 +212,8 @@ const UsedPostUpload: React.FC = () => {
                 type="radio"
                 name="shipping"
                 value="notIncluded"
-                // checked={!shippingIncluded}
-                // onChange={() => setShippingIncluded(false)}
+                checked={!shippingIncluded}
+                onChange={() => setShippingIncluded(false)}
                 className="form-radio"
               />
               <span className="ml-2">배송비 비포함</span>
@@ -88,8 +230,8 @@ const UsedPostUpload: React.FC = () => {
                 type="radio"
                 name="condition"
                 value="new"
-                // checked={productCondition === "new"}
-                // onChange={() => setProductCondition("new")}
+                checked
+                onChange={() => setProductCondition("new")}
                 className="form-radio"
               />
               <span className="ml-2">새상품</span>
@@ -99,8 +241,8 @@ const UsedPostUpload: React.FC = () => {
                 type="radio"
                 name="condition"
                 value="used"
-                // checked={productCondition === "used"}
-                // onChange={() => setProductCondition("used")}
+                checked={productCondition === "used"}
+                onChange={() => setProductCondition("used")}
                 className="form-radio"
               />
               <span className="ml-2">중고상품</span>
@@ -114,9 +256,14 @@ const UsedPostUpload: React.FC = () => {
           <textarea
             className="mt-1 block min-h-[150px] w-full resize-none border border-gray-300 rounded-md p-4 focus:outline-none"
             placeholder="상품 설명을 입력하세요"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
         </div>
-        <button className="w-full h-[45px] py-2 px-4 bg-puple text-white rounded-md">
+        <button
+          onClick={onClickUsedItemUpload}
+          className="w-full h-[45px] py-2 px-4 bg-[#8F5BBD] text-white rounded-md"
+        >
           등록하기
         </button>
       </div>
